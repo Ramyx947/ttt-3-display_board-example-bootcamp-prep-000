9e1222a9fdfffa6574d08551b724d9e80187951e
r1mari@yahoo.com Mironescu <r1mari@yahoo.com>
2018-05-24 10:16:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+ puts 3x3 Tic Tac Tow Board
+ end
+ 
  cell = [" ", " ", " "]
  row = #{cell} " | " #{cell} " | " #{cell}
  puts 3.cell
